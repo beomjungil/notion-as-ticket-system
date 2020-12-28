ea6b9b9aa626143596252a1a795abff03f12f983
beomjungil <gil@beomjun.kr>
2020-12-28 23:31:55 +0900

    style: code formatting with black

--- app/setting.py
+++ app/setting.py
@@ -6,15 +6,16 @@ from yaml import load, FullLoader
 import app.core.notion_client as notion_client
 from app.entities.project import Project
 
-with open('setting.yaml') as file:
+with open("setting.yaml") as file:
     __setting_map__ = load(file, Loader=FullLoader)
-    notion_base_url = 'https://notion.so/'
-    notion_client = notion_client.init(token=__setting_map__.get('notion_token'))
+    notion_base_url = "https://notion.so/"
+    notion_client = notion_client.init(token=__setting_map__.get("notion_token"))
     projects: Dict[str, Project] = {
-        project_map.get('project_prefix'): Project(
+        project_map.get("project_prefix"): Project(
             **project_map,
             collection_view=notion_client.get_collection_view(
-                project_map.get('notion_board_url')
+                project_map.get("notion_board_url")
             )
-        ) for project_map in __setting_map__.get('projects')
+        )
+        for project_map in __setting_map__.get("projects")
     }
