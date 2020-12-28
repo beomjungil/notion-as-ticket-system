4237072a1d7db4990621e3fced270c0ec121809a
beomjungil <gil@beomjun.kr>
2020-12-28 23:30:11 +0900

    fix: replace `notion_helper` with Depends

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,12 +1,10 @@
 from fastapi import HTTPException
 
 from app import setting
-from app.entities.project import Project
 
 
-def get_notion_project(prefix_id: str) -> Project:
+async def get_project_by_prefix(prefix_id: str):
     project = setting.projects.get(prefix_id)
-
     if project is None:
         raise HTTPException(status_code=404, detail="Project not found")
     return project
--- app/routers/go.py
+++ app/routers/go.py
@@ -1,28 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from starlette.responses import RedirectResponse
 
-from app.core.notion_helper import get_notion_project
 import app.setting as setting
 from app.core.expections import NotionException
+from app.dependencies import get_project_by_prefix
+from app.entities.project import Project
+import json
 
 router = APIRouter(
-    prefix='/go'
+    prefix="/go",
 )
 
 
-@router.get('/{prefix_id}')
-def redirect_to_board(prefix_id: str):
-    project = get_notion_project(prefix_id=prefix_id)
+@router.get("/{prefix_id}")
+def redirect_to_board(project: Project = Depends(get_project_by_prefix)):
     return RedirectResponse(url=project.notion_board_url)
 
 
-@router.get('/{prefix_id}/{ticket_id}')
-def redirect_to_board(prefix_id: str, ticket_id: str):
-    project = get_notion_project(prefix_id=prefix_id)
-
+@router.get("/{prefix_id}/{ticket_id}")
+def redirect_to_ticket(
+    ticket_id: str, project: Project = Depends(get_project_by_prefix)
+):
     try:
         ticket = project.query_ticket(ticket_id=ticket_id)
-        notion_url = setting.notion_base_url + ticket.id.replace('-', "")
+        notion_url = setting.notion_base_url + ticket.id.replace("-", "")
         return RedirectResponse(url=notion_url)
     except NotionException:
         return RedirectResponse(url=project.notion_board_url)
